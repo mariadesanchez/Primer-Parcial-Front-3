60847556aad3af08d4fea4c5176e1f8528a88377
mariadesanchez <mariade_snchz@hotmail.com>
2023-05-29 22:55:36 -0300

    Examen Parcial Front 3

---
+++ src/assets/Components/From.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const From = () => {
+  return (
+    <div className="App">
+    <h1>Carga de estudiantes</h1>
+    <form></form>
+    <Card />
+  </div>
+  )
+}
+
+export default From
